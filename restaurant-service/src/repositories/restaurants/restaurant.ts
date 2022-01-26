b86bd5f3689a312fe2fa916b93510f928014628c
AKRMI Selmen <selm3n@gmail.com>
2022-01-26 20:41:44 +0100

    amelioration - wip

--- restaurant-service/src/repositories/restaurants/restaurant.ts
+++ restaurant-service/src/repositories/restaurants/restaurant.ts
@@ -1,35 +1,32 @@
 const config = require('../../../knexfile')
 const knex = require('knex')(config)
+import {IVariablesType, ICountry, IOrdredCountries, IImage, IResp} from '../../interfaces/restaurant'
 
 const { attachPaginate } = require('knex-paginate');
 const { default: axios } = require('axios');
 attachPaginate();
 
-interface a {
-    name: String;
-    haveImageOnly: Boolean
 
-}
 
-const Restaurant = async (_:any, { name , haveImageOnly, perPage, currentPage }) => {
-  if (name !== undefined && haveImageOnly == true) {
+const Restaurant = async (_:any, variables: IVariablesType) => {
+  if (variables.name !== undefined && variables.haveImageOnly == true) {
     return await knex('restaurant')
       .join('restaurant_has_image', 'restaurant.restaurant_uuid', 'restaurant_has_image.restaurant_uuid')
       .select('restaurant.restaurant_uuid as restaurantUuid',
         'restaurant.name as name',
         'restaurant.country_code as country_code',
         knex.raw('ARRAY_AGG (restaurant_has_image.image_uuid) images'))
-      .where('name', 'like', '%' + name + '%')
+      .where('name', 'like', '%' + variables.name + '%')
       .groupBy("restaurant.restaurant_uuid")
       .orderBy("restaurant.restaurant_uuid")
 
-      .paginate({ perPage: perPage ? perPage : 50, currentPage: currentPage ? currentPage : 1 })
-      .then(async function (resp) {
-        let countries = await knex('country')
+      .paginate({ perPage: variables.perPage ? variables.perPage : 50, currentPage: variables.currentPage ? variables.currentPage : 1 })
+      .then(async function (resp:any) {
+        let countries: ICountry[] = await knex('country')
         
-        let ordredCountries = [];
+        let ordredCountries: IOrdredCountries;
 
-        countries.forEach(e => {
+        countries.forEach((e: ICountry) => {
           if (e.country_code == 'UK') {
             ordredCountries.UK = {
               code: e.country_code,
@@ -52,21 +49,18 @@ const Restaurant = async (_:any, { name , haveImageOnly, perPage, currentPage })
 
         const images = await getImages();
 
-        resp.data.forEach(e => {
-          if (e.country_code == 'UK') {
-            e.country = ordredCountries.UK
+        resp.data.forEach((e:IResp) => {
+          
+
+          if( ['ES', 'FR', 'UK'].includes(e.country_code)){
+            e.country = ordredCountries[e.country_code];
           }
-          if (e.country_code == 'FR') {
-            e.country = ordredCountries.FR
-          }
-          if (e.country_code == 'ES') {
-            e.country = ordredCountries.ES
-          }
+          
 
           if (e.images[0] !== null) {
-            let imgs = []
-            e.images.forEach(eim => {
-              images.images.forEach(im => {
+            let imgs:string[] = []
+            e.images.forEach((eim: string) => {
+              images.images.forEach((im:IImage) => {
                 if (im.imageUuid === eim) {
                   imgs.push(im.url)
                 }
@@ -86,7 +80,7 @@ const Restaurant = async (_:any, { name , haveImageOnly, perPage, currentPage })
       })
 
 
-  } else if (name == undefined && haveImageOnly !== true) {
+  } else if (variables.name == undefined && variables.haveImageOnly !== true) {
     return await knex('restaurant')
       .fullOuterJoin('restaurant_has_image', 'restaurant.restaurant_uuid', 'restaurant_has_image.restaurant_uuid')
       .select('restaurant.restaurant_uuid as restaurantUuid',
@@ -96,12 +90,12 @@ const Restaurant = async (_:any, { name , haveImageOnly, perPage, currentPage })
 
       .groupBy("restaurant.restaurant_uuid")
       .orderBy("restaurant.restaurant_uuid")
-      .paginate({ perPage: perPage ? perPage : 50, currentPage: currentPage ? currentPage : 1 })
-      .then(async function (resp) {
+      .paginate({ perPage: variables.perPage ? variables.perPage : 50, currentPage: variables.currentPage ? variables.currentPage : 1 })
+      .then(async function (resp:any) {
         let countries = await knex('country')
-        let ordredCountries = [];
+        let ordredCountries: IOrdredCountries;
 
-        countries.forEach(e => {
+        countries.forEach((e: ICountry) => {
           if (e.country_code == 'UK') {
             ordredCountries.UK = {
               code: e.country_code,
@@ -123,7 +117,7 @@ const Restaurant = async (_:any, { name , haveImageOnly, perPage, currentPage })
         })
 
         const images = await getImages();
-        resp.data.forEach(e => {
+        resp.data.forEach((e:IResp) => {
           if (e.country_code == 'UK') {
             e.country = ordredCountries.UK
           }
